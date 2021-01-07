40b4fa3a43db193e626316482596eb16c1458195
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-01-07 00:14:15 -0500

    SearchBar interactivity

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,38 @@
 import React from 'react';
 import './App.css';
 import Business from '../Business/Business';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imgSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business, 
+  business,
+  business,
+  business,
+  business,
+  business
+]; 
 
 function App() {
-  return <Business />;
+  return (  
+  <div className="App">
+    <h1>Famished</h1>
+    <SearchBar />
+    <BusinessList businesses={businesses} />
+  </div>
+  );
 
 }
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,10 +8,40 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        };
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({
+            sortBy: sortByOption
+        })
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map((sortByOption) => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map((sortByOption) => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li className={this.getSortByClass(sortByOptionValue)} 
+                       onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                       key={sortByOptionValue}>{sortByOption}</li>
         });
     }
 
