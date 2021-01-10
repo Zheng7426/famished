620b5bb08211af20591b80c9cdbd9069a16ac5f1
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-01-09 22:23:40 -0500

    Yelp fetch module

---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+import apiKey from './secrets';
+
+const baseUrl = 'https://api.yelp.com/v3/businesses/search';
+const corsUrl = 'https://cors-anywhere.herokuapp.com/';
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`${corsUrl}${baseUrl}?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: {
+                Authorization: `Bearer ${apiKey}` 
+            }
+        }).then((response) => response.json()).then((jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map((business) => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                        category: business.categories[0].title
+                    };
+                });
+            }
+        });
+    }
+};
+
+export default Yelp;
